6025ba5e85c01c1e9aaa0e4bbbddc77917fb4c4f
SWARA518 <63648720+SWARA518@users.noreply.github.com>
2020-06-03 15:49:17 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,13 @@
+class Block{
+  constructor(x,y,width,height) {
+    
+    this.body = Bodies.rectangle(x,y,width,height);
+    this.width = width;
+    this.height = height;
+    World.add(world, this.body);
+  }
+  display(){
+    rectMode(CENTER);
+    rect(pos.x, pos.y, this.width, this.height);
+  }
+}
