82459ed9f123648a650d6dbcfa819663dc9b82b1
SWARA518 <63648720+SWARA518@users.noreply.github.com>
2020-06-09 16:33:45 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,14 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine,world;
+
 function setup() {
   createCanvas(800,400);
+  engine = Engine.create;
+  world = engine.world;
   
   //level one [four blocks]
   block1 = new Block(330,235,30,40);
